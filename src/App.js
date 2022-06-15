35a5ac4fa3a3b74ba3e3308ea8f20df95dd994e6
nathan-miranda-soma <nathan.miranda@somagrupo.com.br>
2022-06-14 22:00:59 -0300

    Create Counter

--- src/App.js
+++ src/App.js
@@ -1,37 +1,117 @@
-import { useState } from "react";
+import { Component } from "react";
+import { useState, useEffect } from "react";
 
-import FormTask from "./components/molecules/form-task";
-import TaskList from "./components/organisms/task-list";
+class OutroApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      isCounting: false,
+      intervalId: 0,
+    };
+    this.zerarAContagem = this.zerarAContagem.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
 
-import "./App.css";
+  handleOnClick() {
+    this.setState((oldState) => ({
+      ...oldState,
+      isCounting: !oldState.isCounting,
+    }));
+  }
 
-export default function App() {
-  const [tasks, setTasks] = useState([{ title: "Titulo da Tarefa", id: "0" }]);
+  zerarAContagem() {
+    this.setState((oldState) => ({
+      ...oldState,
+      count: 0,
+    }));
+  }
 
-  const addNewTask = function (newTask) {
-    setTasks(function (oldTaskList) {
-      const newId = `${oldTaskList.length}`;
-      const newTasks = [...oldTaskList];
+  componentDidUpdate(prevPros, prevState) {
+    if (prevState.isCounting === this.state.isCounting) {
+      return;
+    }
 
-      newTasks.push({
-        ...newTask,
-        id: newId,
-      });
+    if (this.state.isCounting) {
+      let intervalId = window.setInterval(() => {
+        this.setState((oldState) => ({
+          ...oldState,
+          count: oldState.count + 1,
+        }));
+      }, 1000);
 
-      return newTasks;
-    });
+      this.setState((oldState) => ({
+        ...oldState,
+        intervalId: intervalId,
+      }));
+    } else {
+      window.clearInterval(this.state.intervalId);
+    }
+  }
+
+  render() {
+    let acaoDoBotao;
+
+    if (this.state.isCounting) {
+      acaoDoBotao = "Parar";
+    } else if (this.state.count > 0) {
+      acaoDoBotao = "Recomeçar";
+    } else {
+      acaoDoBotao = "Começar";
+    }
+
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1 style={{ fontSize: 30 }}>{this.state.count}</h1>
+        <button onClick={this.handleOnClick}>{acaoDoBotao} a Contagem</button>
+        <button onClick={this.zerarAContagem}>Zerar a Contagem</button>
+      </div>
+    );
+  }
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+    if (isCounting) {
+      intervalId = window.setInterval(() => {
+        setCount((oldState) => oldState + 1);
+      }, 1000);
+    }
+
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, [isCounting]);
+
+  const handleOnClick = () => {
+    setIsCounting((oldState) => !oldState);
   };
 
-  const removeTask = function (id) {
-    setTasks((oldTaskList) => {
-      return oldTaskList.filter((task) => task.id !== id);
-    });
+  const zerarAContagem = () => {
+    setCount(0);
   };
+
+  let acaoDoBotao;
+
+  if (isCounting) {
+    acaoDoBotao = "Parar";
+  } else if (count > 0) {
+    acaoDoBotao = "Recomeçar";
+  } else {
+    acaoDoBotao = "Começar";
+  }
 
   return (
-    <div style={{ maxWidth: 500, margin: "auto" }}>
-      <FormTask onSubmit={addNewTask} />
-      <TaskList tasks={tasks} removeTask={removeTask} />
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ fontSize: 30 }}>{count}</h1>
+      <button onClick={handleOnClick}>{acaoDoBotao} a Contagem</button>
+      <button onClick={zerarAContagem}>Zerar a Contagem</button>
     </div>
   );
 }
+
+export default OutroApp;
